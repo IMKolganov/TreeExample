e9c5cfc79cff99df9c0c6ec20aa2233ea4e5bcdf
Ivan <110064602+IMKolganov@users.noreply.github.com>
2023-05-19 12:42:37 +0300

    add migration

--- DB/Repositories/ActionRepository.cs
+++ DB/Repositories/ActionRepository.cs
@@ -1,51 +1,51 @@
-﻿// using Microsoft.EntityFrameworkCore;
-// using DB.Entities;
-// using DB.Repositories.Interfaces;
-// using Entities = DB.Entities.Action;
-//
-// namespace DB.Repositories;
-//
-// public class ActionRepository : IActionRepository
-// {
-//     private readonly ApplicationDbContext _context;
-//     private readonly DbSet<Entities.Action> _dbSet;
-//
-//     public ActionRepository(ApplicationDbContext context)
-//     {
-//         _context = context;
-//         _dbSet = context.Set<Entities.Action>();
-//     }
-//
-//     public async Task<Entities.Action?> Get(int id)
-//     {
-//         var result = await _dbSet.FindAsync(id);
-//         return result;
-//     }
-//     
-//     public async Task<Entities.Action> Add(Entities.Action action)
-//     {
-//         await _dbSet.AddAsync(action);
-//         await _context.SaveChangesAsync();
-//         return action;
-//     }
-//     
-//     public async Task<Entities.Action> Update(Entities.Action action)
-//     {
-//         _dbSet.Update(action);
-//         await _context.SaveChangesAsync();
-//         return action;
-//     }
-//
-//     public async Task<List<Entities.Action>> GetAll()
-//     {
-//         return await _dbSet.ToListAsync();
-//     }
-//     
-//     public async Task<List<Entities.Action>> GetByIds(List<int> ids)
-//     {
-//         var result = await _dbSet
-//             .Where(a => ids.Contains(a.Id))
-//             .ToListAsync();
-//         return result;
-//     }
-// }
+﻿using Microsoft.EntityFrameworkCore;
+using DB.Entities;
+using DB.Repositories.Interfaces;
+using Entities = DB.Entities.Action;
+
+namespace DB.Repositories;
+
+public class ActionRepository : IActionRepository
+{
+    private readonly ApplicationDbContext _context;
+    private readonly DbSet<Entities.Action> _dbSet;
+
+    public ActionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<Entities.Action>();
+    }
+
+    public async Task<Entities.Action?> Get(int id)
+    {
+        var result = await _dbSet.FindAsync(id);
+        return result;
+    }
+    
+    public async Task<Entities.Action> Add(Entities.Action action)
+    {
+        await _dbSet.AddAsync(action);
+        await _context.SaveChangesAsync();
+        return action;
+    }
+    
+    public async Task<Entities.Action> Update(Entities.Action action)
+    {
+        _dbSet.Update(action);
+        await _context.SaveChangesAsync();
+        return action;
+    }
+
+    public async Task<List<Entities.Action>> GetAll()
+    {
+        return await _dbSet.ToListAsync();
+    }
+    
+    public async Task<List<Entities.Action>> GetByIds(List<int> ids)
+    {
+        var result = await _dbSet
+            .Where(a => ids.Contains(a.Id))
+            .ToListAsync();
+        return result;
+    }
+}
--- TreeExample/Program.cs
+++ TreeExample/Program.cs
@@ -1,3 +1,11 @@
+using BusinessLayer.Services;
+using BusinessLayer.Services.Interfaces;
+using BusinessLayer.Services.Utils;
+using DB;
+using DB.Repositories;
+using DB.Repositories.Interfaces;
+using TreeExample;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add context
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContext");
+builder.Services.AddSqlServer<ApplicationDbContext>(connectionString);
+builder.Services.AddScoped<IMigrateDbCtrl, MigrateDbCtrl>();
+
+builder.Services.AddScoped<IExcelImport, ExcelImport>();
+builder.Services.AddScoped<IExcelExportGeneric, ExcelExportGeneric>();
+
+
+builder.Services.AddScoped<IActionRepository, ActionRepository>();
+builder.Services.AddScoped<IApplicationRepository, ApplicationRepository>();
+builder.Services.AddScoped<ITreeViewRepository, TreeViewRepository>();
+
+
+builder.Services.AddScoped<IPermissionService, PermissionService>();
+
+// Add MapperConfig
+builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 var app = builder.Build();
 
